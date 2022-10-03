8a563b0a1a77af4dd5ecfb1521646cb913c14225
JoanaPagliarin <joanapagliarin5@gmail.com>
2022-10-03 12:56:18 -0300

    terminar código

---
+++ Exercícios avulsos/exAulaFruta.js
@@ -0,0 +1,57 @@
+function imprime(i)
+{
+        let quantidade = parseInt(frutas.length);
+        console.log("Quantidade de frutas cesta " + i + "= " + quantidade);
+}
+
+while (i <  6)
+{
+    if ()
+    let i=0;
+    imprime(i);
+    i++;
+}
+
+
+function contador(cesta){
+    qtd=cesta.frutas.length;
+    let i, totaldefrutas=0;
+    for (i=0;i<qtd;i++){
+        totaldefrutas=totaldefrutas+cesta.frutas[i].quantidade;
+        
+    }
+    console.log("O total de frutas eh: "+totaldefrutas);
+    }
+    contador(cesta1)
+ 
+
+const cesta1={
+    proprietario: "Rodolfo",
+    material: "vime",
+    frutas:[fruta1,fruta4,fruta5]
+}
+const cesta2={
+    proprietario: "Joseph Stalin",
+    material: "plástico",
+    frutas:[fruta1,fruta2,fruta3]
+}
+const cesta3={
+    proprietario: "Hugo Chavez",
+    material: "palha",
+    frutas:[fruta4,fruta5,fruta6]
+}
+const cesta4={
+    proprietario: "Mussolini",
+    material: "madeira",
+    frutas:[fruta2,fruta4,fruta6]
+}
+const cesta5={
+    proprietario: "Fuhrer",
+    material: "ferro",
+    frutas:[fruta3,fruta5,fruta6]
+}
+const cesta6={
+    proprietario: "Armstrong",
+    material: "nanomáquinas",
+    frutas:[fruta1,fruta2,fruta3,fruta4,fruta5,fruta6]
+}
